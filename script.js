f569e75c50bfb7f35667a004036279a9d86cb57f
Yash Kamaat <yashtkamaat@gmail.com>
2021-10-20 08:29:57 -0400

    boo, and candy goal

---
+++ script.js
@@ -0,0 +1,39 @@
+// FUNCTION EXERCISE
+
+// 1
+spookyGhost = () => {
+    console.log(`BOO!!!`);
+}
+console.log("==================")
+// 2
+function candy(goal){
+    console.log({`$goal`}, {`$goal`}, {`$goal`}, {`$goal`} );
+}
+candy(`GET CANDY`);
+console.log("==================")
+// 3
+
+console.log("==================")
+// 4
+console.log("==================")
+// 5
+console.log("==================")
+// 6
+
+console.log("==================")
+// 7
+console.log("==================")
+// 8
+console.log("==================")
+// 9
+console.log("==================")
+// 10
+console.log("==================")
+// 11a
+console.log("==================")
+// 11b
+console.log("==================")
+
+// BONUS
+// 12a
+console.log("==================")
