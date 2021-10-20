edcda336d67e554b80a3dc7b84c780a76ba5a3b2
Yash Kamaat <yashtkamaat@gmail.com>
2021-10-20 08:54:50 -0400

    5Logged 5, 6, and starting 7

--- script.js
+++ script.js
@@ -6,23 +6,51 @@ spookyGhost = () => {
 }
 console.log("==================")
 // 2
-function candy(goal){
+function candy(goal) {
     console.log(`${goal}, ${goal}, ${goal}, ${goal}`);
 }
 candy(`GET CANDY`);
 console.log("==================")
 
 // 3
-
+function werewolf(a,b) {
+    if (a=== "Full" && b === "Moon"){
+        console.log("ARGH-WOOOO!");
+    }
+}
+werewolf("Full", "Moon");
 console.log("==================")
 // 4
+function halloween() {
+return `October 31st`;
+}
+console.log(halloween());
 console.log("==================")
 // 5
+function isEnoughCandy(pieces){
+    if (pieces > 31){
+        return true;
+    } else if (pieces < 31){
+        return false;
+    }
+};
+console.log(isEnoughCandy(31));
 console.log("==================")
 // 6
-
+function candyCounter(nums){
+    let sum= 0;
+    for (i = 0; i < nums.length; i++) {
+        sum = sum +nums[i];
+    }
+    return sum;
+};
+candyCounter([4,7,9]);
+console.log(candyCounter([4,7,9]));
 console.log("==================")
 // 7
+const hauntedMansion = function() {
+
+}
 console.log("==================")
 // 8
 console.log("==================")
